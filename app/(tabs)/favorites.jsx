51377231190dc0dbaf35ba05bd791a1ada7a2636
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-08-19 15:51:59 +0300

    Implement favorite tab logic

--- app/(tabs)/favorites.jsx
+++ app/(tabs)/favorites.jsx
@@ -1,9 +1,86 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  Alert,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import { useState, useEffect } from 'react';
+import { useClerk, useUser } from '@clerk/clerk-expo';
+import { API_URL } from '@/constants/api';
+import { favoritesStyles } from '@/assets/styles/favorites.styles';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '@/constants/colors';
+import RecipeCard from '@/components/RecipeCard';
+import NoFavoritesFound from '@/components/NoFavoritesFound';
+import LoadingSpinner from '@/components/LoadingSpinner';
 
 const FavoritesScreen = () => {
+  const { signOut } = useClerk();
+  const { user } = useUser();
+  const [favoriteRecepies, setFavoriteRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadFavorites = async () => {
+      try {
+        const responce = await fetch(`${API_URL}/favorites/${user.id}`);
+
+        if (!responce.ok) throw new Error('Failed to fetch favorites');
+        const favorites = await responce.json();
+
+        // transform the data to mach the RecipeCard component's expected format
+        const transformedFavorites = favorites.data.map((favorite) => ({
+          ...favorite,
+          id: favorite.recipeId,
+        }));
+
+        setFavoriteRecipes(transformedFavorites);
+      } catch (error) {
+        console.log('Error loading favorites', error);
+        Alert.alert('Error', 'Failed to load favorites');
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadFavorites();
+  }, [user.id]);
+
+  const handleSignOut = () => {
+    Alert.alert('Logout', 'Are you sure you want to logout', [
+      { text: 'Cancel', style: 'cancel' },
+      { text: 'Logout', style: 'destructive', onPress: signOut },
+    ]);
+  };
+
+  if (loading) return <LoadingSpinner message="Loading your favorites..." />;
+
   return (
-    <View>
-      <Text>FavoritesScreen</Text>
+    <View style={favoritesStyles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={favoritesStyles.header}>
+          <Text style={favoritesStyles.title}>Favorites</Text>
+          <TouchableOpacity
+            style={favoritesStyles.logoutButton}
+            onPress={handleSignOut}
+          >
+            <Ionicons name="log-out-outline" size={22} color={COLORS.text} />
+          </TouchableOpacity>
+        </View>
+        <View style={favoritesStyles.recipesSection}>
+          <FlatList
+            data={favoriteRecepies}
+            renderItem={({ item }) => <RecipeCard recipe={item} />}
+            keyExtractor={(item) => item.id.toString()}
+            numColumns={2}
+            columnWrapperStyle={favoritesStyles.row}
+            contentContainerStyle={favoritesStyles.recipesGrid}
+            scrollEnabled={false}
+            ListEmptyComponent={<NoFavoritesFound />}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
---
+++ constants/api.js
@@ -0,0 +1,2 @@
+// export const API_URL = 'http://localhost:5001/api';
+export const API_URL = 'https://recipe-app-ljrx.onrender.com/api';
